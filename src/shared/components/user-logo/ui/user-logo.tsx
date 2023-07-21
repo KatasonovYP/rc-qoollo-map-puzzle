f304725510cd4eab5d3d84bfa8518bc0bffa25bc
Yury Katasonov <katasonov-yury@yandex.ru>
2023-07-21 11:02:18 +0300

    fix user logo

--- src/shared/components/user-logo/ui/user-logo.tsx
+++ src/shared/components/user-logo/ui/user-logo.tsx
@@ -1,11 +1,13 @@
-import type { FC, ReactNode } from 'react';
+import type { FC } from 'react';
 import cls from './user-logo.module.css';
+import cameraUrl from 'shared/assets/images/camera_50.png';
 
 export const UserLogo: FC = () => {
+	const userLogoUrl = localStorage.getItem('user_photo') || cameraUrl;
 	return (
 		<img
-			className={`${cls.user_logo}`}
-			src={localStorage.getItem('user_photo')}
+			className={cls.user_logo}
+			src={userLogoUrl}
 			alt='User photo'
 		></img>
 	);
